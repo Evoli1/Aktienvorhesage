5fbc4557ec2477fb0e90843d434ab413d8e09e12
Linh Le <94382388+Evoli1@users.noreply.github.com>
2021-11-28 05:49:56 +0100

    Update Sentiment Analysis Headlines News.py

--- Sentiment Analysis Headlines News.py
+++ Sentiment Analysis Headlines News.py
@@ -1,24 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#Quelle: https://thecodex.me/blog/sentiment-analysis-tool-for-stock-trading
 pip install bs4
-
-
-# In[37]:
-
 
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
-# In[38]:
-
 
 import ssl
 
@@ -29,10 +16,7 @@ except AttributeError:
 else:
     ssl._create_default_https_context = _create_unverified_https_context
 
-
-# In[58]:
-
-
+#Gathering FinViz Data
 finviz_url = 'https://finviz.com/quote.ashx?t='
 tickers = ['MSFT', 'GOOG', 'AAPL']
 
@@ -47,13 +31,10 @@ for ticker in tickers:
     html = BeautifulSoup(response, features='html.parser')
     news_table = html.find(id='news-table')
     news_tables[ticker] = news_table
+    
 
-
-# In[59]:
-
-
+#Parsing FinViz Data
 parsed_data = []
-
 for ticker, news_table in news_tables.items():
 
     for row in news_table.findAll('tr'):
@@ -70,26 +51,18 @@ for ticker, news_table in news_tables.items():
         parsed_data.append([ticker, date, time, title])
 
 df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
-df
 
 
-# In[60]:
-
-
+#Applying Sentiment Analysis
 vader = SentimentIntensityAnalyzer()
 
 f = lambda title: vader.polarity_scores(title)['compound']
 df['compound'] = df['title'].apply(f)
 df['date'] = pd.to_datetime(df.date).dt.date
+
+#Visualizing the Results in MatPlotLib
 plt.figure(figsize=(10,8))
 mean_df = df.groupby(['ticker', 'date']).mean().unstack()
 mean_df = mean_df.xs('compound', axis="columns").transpose()
 mean_df.plot(kind='bar')
 plt.show()
-
-
-# In[ ]:
-
-
-
-
