fc2aaf828b856e71362fc090cb3b706e081420ae
joket001 <84315589+joket001@users.noreply.github.com>
2021-11-29 12:06:54 +0100

    Update data_api.py

--- data_api.py
+++ data_api.py
@@ -1,9 +1,42 @@
 import yfinance as yf
+import pandas as pd
+import numpy as np
+import talib as ta
+from datetime import date, timedelta
 
-# Tricker Objekt fuer Microsoft Cooperation "MSFT" Aktie
-msft = yf.Ticker("MSFT")
+# Analysezeitraum Heute - 729 Tage 
+start = date.today() - timedelta(days=729)
+end = date.today()
 
-# Historische Daten aus der Datenbank herrunterladen
-hist = msft.history(period="5y", interval="1d" )
+# Download der FANG Aktiendaten
+assets = ["FB", "AMZN", "AAPL", "NFLX", "GOOG"]
+asset_list = list()     # Liste der Dataframes
+for asset in assets:
+    asset = yf.download(
+        tickers = asset,
+        start = start,
+        end = end,
+        # fetch data by interval (including intraday if period < 60 days)
+        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+        # (optional, default is '1d')
+        interval = "1d"
+        )
+    asset_list.append(asset)
+    
+# Technische Indikatoren zum trainieren des Modells
+# Periods sind auf Standardwerten
+def indikator(asset):
+    asset["RSI"] = ta.RSI(asset["Close"], timeperiod = 14)                                  # Relativer Staerke Index
+    asset["ROC"] = ta.ROC(asset["Close"], timeperiod = 10)                                  # Rate Of Change
+    asset["%R"] = ta.WILLR(asset["High"],asset["Low"], asset["Close"], timeperiod = 14)     # William’s Prozent Range
+    asset["OBV"] = ta.OBV(asset["Close"], asset["Volume"])                                  # On -Balance Volume
+    asset["MACD"], asset["MACD_SIGNAL"], asset["MACD_HIST"] = ta.MACD(asset["Close"], fastperiod = 12, slowperiod = 26, signalperiod = 9 )    # Moving Average Convergence/Divergence
+    
+    # Vorhersagewerte - Wenn der Wert in 5 Tagen hoeher ist als heute dann 1 ansonsten -1
+    asset["Prediction"] = np.where(asset["Close"].shift(-5) > asset["Close"], 1, -1)
 
-print(hist)
+# Indikatorfunktion ausfuehren
+for asset in asset_list:
+    indikator(asset)
+
+print(asset_list[1])
