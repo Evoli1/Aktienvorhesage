83d4ba7633221db70adefc46648b96ace8c39659
Linh Le <94382388+Evoli1@users.noreply.github.com>
2022-01-08 17:54:19 +0100

    Update Sentiment Analysis Headlines News.py

--- Sentiment Analysis Headlines News.py
+++ Sentiment Analysis Headlines News.py
@@ -1,13 +1,23 @@
-#Quelle: https://thecodex.me/blog/sentiment-analysis-tool-for-stock-trading
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 import matplotlib.pyplot as plt
+import nltk
+import ssl
 
-#Gathering FinViz Data
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('vader_lexicon')
+
+#Download 100 aktuell Header von jeder Aktie
 finviz_url = 'https://finviz.com/quote.ashx?t='
-tickers = ['FB', 'AMZN', 'AAPL', 'NFLX', 'GOOG']
+tickers = ['FB','AMZN','AAPL','NFLX','GOOG']
 
 news_tables = {}
 
@@ -21,8 +31,7 @@ for ticker in tickers:
     news_table = html.find(id='news-table')
     news_tables[ticker] = news_table
     
-
-#Parsing FinViz Data
+#Erstellen Dataframe für Analyse 
 parsed_data = []
 for ticker, news_table in news_tables.items():
 
@@ -41,13 +50,9 @@ for ticker, news_table in news_tables.items():
 
 df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
 
-# Einstellen der Spalte 'Datum' als Indexspalte
-df.set_index('date', inplace= True)
-
-#Applying Sentiment Analysis
+#Calculating Sentiment Scores
 vader = SentimentIntensityAnalyzer()
-
-#Displaying results in table
+#Anzeigen Ergebnisse von compound-Punkte in Tabelle
 df['compound'] = ''
 df['negative'] = ''
 df['neutral'] = ''
@@ -58,12 +63,101 @@ df['negative'] = df['title'].apply(lambda x: vader.polarity_scores(x)['neg'])
 df['neutral'] = df['title'].apply(lambda x: vader.polarity_scores(x)['neu'])
 df['positive'] = df['title'].apply(lambda x: vader.polarity_scores(x)['pos'])
 
-print(df)
-
-#Visualizing the Results in MatPlotLib
-df['date'] = pd.to_datetime(df.date).dt.date
+#Grafische Darstellung der Analyse 
+df['Date'] = pd.to_datetime(df.date).dt.date
 plt.figure(figsize=(10,8))
-mean_df = df.groupby(['ticker', 'date']).mean().unstack()
+mean_df = df.groupby(['ticker','Date']).mean().unstack()
 mean_df = mean_df.xs('compound', axis="columns").transpose()
-mean_df.plot(kind='bar')
+mean_df.plot(kind="bar")
+plt.show()
+
+##Vorhersagemodell aufbauen, z.B für APPL
+parsed_text = []
+ticker = "APPL"
+for row in news_table.findAll('tr'):
+
+        title = row.a.text
+        date_data = row.td.text.split(' ')
+
+        if len(date_data) == 1:
+            time = date_data[0]
+        else:
+            date = date_data[0]
+            time = date_data[1]
+
+        parsed_text.append([ticker, date, time, title])
+
+df_ml = pd.DataFrame(parsed_text, columns=['ticker', 'date', 'time', 'title'])
+
+#Re-Formatierung der Zeitdaten fürs Kombinieren zwei Datensätzen
+import locale
+from datetime import datetime
+locale.setlocale(locale.LC_TIME, locale.normalize("en"))
+df_ml['date']=pd.to_datetime(df_ml['date'], format='%b-%d-%y')
+
+#Calculating Sentiment Scores
+vader = SentimentIntensityAnalyzer()
+df_ml['compound'] = ''
+df_ml['negative'] = ''
+df_ml['neutral'] = ''
+df_ml['positive'] = ''
+df_ml['compound'] = df_ml['title'].apply(lambda x: vader.polarity_scores(x)['compound'])
+df_ml['negative'] = df_ml['title'].apply(lambda x: vader.polarity_scores(x)['neg'])
+df_ml['neutral'] = df_ml['title'].apply(lambda x: vader.polarity_scores(x)['neu'])
+df_ml['positive'] = df_ml['title'].apply(lambda x: vader.polarity_scores(x)['pos'])
+
+#Durchschnittliche compound-Punkte jedes Tages 
+df_ml = df_ml[['date','compound']]
+df_ml = df_ml.groupby(['date'])
+ml_average = df_ml.sum()/df_ml.count()
+
+#Aktienkursendaten
+from datetime import date, timedelta
+import yfinance as yf
+from yfinance import tickers, ticker
+
+start = date.today() - timedelta(days=365)
+end = date.today()
+datalist = yf.download('FB', 
+                      start= start, 
+                      end= end, 
+                      progress=False,
+)
+datalist[['Close']]
+df_data = pd.DataFrame(datalist)
+df_data = df_data.sort_index(ascending=False, axis=0)
+
+#Kombinieren zwei Datensätzen
+combi = pd.concat([ml_average, df_data], axis=1)
+combi = combi.sort_index(ascending=False, axis=0)
+#Bearbeitung des kombinierten Datensatzes
+combi = combi[['Close','compound']]
+combi.dropna(inplace=True)
+
+#Einstellung Variablen und Zielwert
+x = combi.iloc[:, 0:1].values 
+y = combi.iloc[:, 1].values 
+# Anpassen der Random-Forest-Regression an den Datensatz
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
+regressor.fit(x, y)  
+Y_pred = regressor.predict(np.array([6.5]).reshape(1, 1)) #Output-testen bei der Wertenänderung
+
+# Visualisierung der Ergebnisse der Random Forest Regression
+
+
+# Erstellen eines Wertebereichs vom Mindestwert von x bis zum Höchstwert von x mit einer Differenz von 0,01 zwischen zwei aufeinanderfolgenden Werten
+X_grid = np.arange(min(x), max(x), 0.01)
+
+X_grid = X_grid.reshape((len(X_grid), 1))
+
+# Streudiagramm für Originaldaten
+plt.scatter(x, y, color = 'blue')
+
+# Darstellung die prognostizierte Daten grafisch
+plt.plot(X_grid, regressor.predict(X_grid),
+        color = 'green')
+plt.title('Random Forest Regression')
+plt.xlabel('Close')
+plt.ylabel('Sentiment')
 plt.show()
